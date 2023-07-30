218be6a85ecb13f7ecc3fbbfded52e37fa0e5097
gusflus <gus.flusser@gmail.com>
2023-07-30 13:22:12 -0700

    works

--- Board.py
+++ Board.py
@@ -1,6 +1,112 @@
+import random
+
 class Board:
     def __init__(self):
-        self.state = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+        self.state = [[0 for _ in range(4)] for _ in range(4)]
         self.score = 0
         self.game_over = False
         self.won = False
+
+    def reset(self):
+        self.state = [[0 for _ in range(4)] for _ in range(4)]
+        self.score = 0
+        self.game_over = False
+        self.won = False
+
+
+    # can you make a function that slides the 2048 board up?
+
+
+    def slide_up(self):
+        move_occurred = False
+        for col in range(4):
+            for row in range(1, 4):
+                if self.state[row][col]:
+                    r = row
+                    while r > 0:
+                        if self.state[r - 1][col] == 0:
+                            self.state[r - 1][col] = self.state[r][col]
+                            self.state[r][col] = 0
+                            r -= 1
+                            move_occurred = True
+                        elif self.state[r - 1][col] == self.state[r][col]:
+                            self.state[r - 1][col] *= 2
+                            self.score += self.state[r - 1][col]
+                            self.state[r][col] = 0
+                            move_occurred = True
+                            break
+                        else:
+                            break
+        return move_occurred
+
+    def slide_left(self):
+        move_occurred = False
+        for row in range(4):
+            for col in range(1, 4):
+                if self.state[row][col]:
+                    c = col
+                    while c > 0:
+                        if self.state[row][c - 1] == 0:
+                            self.state[row][c - 1] = self.state[row][c]
+                            self.state[row][c] = 0
+                            c -= 1
+                            move_occurred = True
+                        elif self.state[row][c - 1] == self.state[row][c]:
+                            self.state[row][c - 1] *= 2
+                            self.score += self.state[row][c - 1]
+                            self.state[row][c] = 0
+                            move_occurred = True
+                            break
+                        else:
+                            break
+        return move_occurred
+
+    def slide_right(self):
+        move_occurred = False
+        for row in range(4):
+            for col in range(2, -1, -1):
+                if self.state[row][col]:
+                    c = col
+                    while c < 3:
+                        if self.state[row][c + 1] == 0:
+                            self.state[row][c + 1] = self.state[row][c]
+                            self.state[row][c] = 0
+                            c += 1
+                            move_occurred = True
+                        elif self.state[row][c + 1] == self.state[row][c]:
+                            self.state[row][c + 1] *= 2
+                            self.score += self.state[row][c + 1]
+                            self.state[row][c] = 0
+                            move_occurred = True
+                            break
+                        else:
+                            break
+        return move_occurred
+
+    def slide_down(self):
+        move_occurred = False
+        for col in range(4):
+            for row in range(2, -1, -1):
+                if self.state[row][col]:
+                    r = row
+                    while r < 3:
+                        if self.state[r + 1][col] == 0:
+                            self.state[r + 1][col] = self.state[r][col]
+                            self.state[r][col] = 0
+                            r += 1
+                            move_occurred = True
+                        elif self.state[r + 1][col] == self.state[r][col]:
+                            self.state[r + 1][col] *= 2
+                            self.score += self.state[r + 1][col]
+                            self.state[r][col] = 0
+                            move_occurred = True
+                            break
+                        else:
+                            break
+        return move_occurred
+
+    def _generate_new_tile(self):
+        empty_cells = [(i, j) for i in range(len(self.state)) for j in range(len(self.state[i])) if self.state[i][j] == 0]
+        if empty_cells:
+            i, j = random.choice(empty_cells)
+            self.state[i][j] = 2 if random.random() < 0.9 else 4
--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import pygame
-import Board
+from Board import Board
 
 pygame.init()
 screen = pygame.display.set_mode((500, 600))
@@ -7,7 +7,13 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
+pressed_up = False
+pressed_down = False
+pressed_left = False
+pressed_right = False
+
 board = Board()
+board._generate_new_tile()
 
 tile_colors = {
     0: (199, 199, 199),
@@ -26,7 +32,7 @@ tile_colors = {
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-def draw_tile(x, y, value):
+def draw_tile(y, x, value):
     pygame.draw.rect(screen, (199, 199, 199), pygame.Rect(35 + 110 * x, 135 + 110 * y, 100, 100))
     if value != 0:
         pygame.draw.rect(screen, tile_colors[value], pygame.Rect(35 + 110 * x, 135 + 110 * y, 100, 100))
@@ -43,21 +49,53 @@ while running:
 
     screen.fill((199, 199, 199))
 
+    titleFont = pygame.font.Font('freesansbold.ttf', 46)
+    title = titleFont.render("2048", True, (0, 0, 0))
+    titleRect = title.get_rect()
+    titleRect.center = (screen.get_width() / 2, 55)
+    screen.blit(title, titleRect)
+    
+    font = pygame.font.Font('freesansbold.ttf', 28)
+    text = font.render("Score: " + str(board.score), True, (0, 0, 0))
+    textRect = text.get_rect()
+    textRect.center = (3 * screen.get_width() / 4 + 10, 60)
+    screen.blit(text, textRect)
+
     # pygame.draw.text(screen, "Score: 0", (25, 25))
     pygame.draw.rect(screen, (148, 148, 148), pygame.Rect(25, 125, 450, 450))
     for i in range(4):
         for j in range(4):
             draw_tile(i, j, board.state[i][j])
+            # print('[' + str(i) + '][' + str(j) + ']: ' + str(board.state[i][j]))
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+
+    if not pressed_up and keys[pygame.K_w]:
+        board.slide_up() and board._generate_new_tile()
+        pressed_up = True
+    elif not keys[pygame.K_w]:
+        pressed_up = False
+
+    if not pressed_down and keys[pygame.K_s]:
+        board.slide_down() and board._generate_new_tile()
+        pressed_down = True
+    elif not keys[pygame.K_s]:
+        pressed_down = False
+
+    if not pressed_left and keys[pygame.K_a]:
+        board.slide_left() and board._generate_new_tile()
+        pressed_left = True
+    elif not keys[pygame.K_a]:
+        pressed_left = False
+
+    if not pressed_right and keys[pygame.K_d]:
+        board.slide_right() and board._generate_new_tile()
+        pressed_right = True
+    elif not keys[pygame.K_d]:
+        pressed_right = False
+
+    if keys[pygame.K_r]:
+        board.reset()
 
     pygame.display.flip()
 
